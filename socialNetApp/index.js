f05ad3f38847ad32246f17239455da5faf45002d
Ranjeet <ranjeetkumar692@gmail.com>
2015-07-17 12:00:12 +0530

    SearchOn to SearchQuery

--- socialNetApp/index.js
+++ socialNetApp/index.js
@@ -17,35 +17,35 @@ $(document).ready(function() {
 			$("#noticeBar").empty().append("Your friend shared a post");
 		},
 		{
-			success : function(){ 
+			success : function(){
 				console.log("CloudObject.on executed");
-				
+
 			},
 			error : function(err){
 				console.log("error in CloudObect.on");
-			} 
-		}); 
-		
-		//creating an object for 'feed' 
+			}
+		});
+
+		//creating an object for 'feed'
 		obj = new CB.CloudObject("feed");
-		
+
 		obj.isSearchable = true;
-		
+
 		//creating an object for querying 'feed' data
-		var query = new CB.CloudQuery("feed"); 
+		var query = new CB.CloudQuery("feed");
 		query.orderByDesc('createdAt');
 		query.setLimit(70);
-		query.find({ 
+		query.find({
 			//query will return a list of CloudObjects.
-			success: function(list){ 
+			success: function(list){
 				//extracting and appending data into html page from list of CloudObjects
-				for (i = 0; i < list.length; i++) { 
+				for (i = 0; i < list.length; i++) {
 					$("#feed").append("<div class='panel panel-default'><div class='panel-body' style=''><div class='col-md-1 col-sm-2 col-xs-3'><img src='image/profile.png' class='img-responsive img-circle'/></div><div><strong>"+list[i].get('name')+"</strong><br/>"+list[i].get('post')+"</div></div></div>");
 				}
 			},
-			error: function(err){ 
+			error: function(err){
 				console.log("unable to fetch data");
-			} 
+			}
 		});
 });
 
@@ -58,26 +58,27 @@ $("#postForm").submit(function() {
 	obj.set("name", $("#name").val());
 	obj.set("post",$("#text").val());
 	//saving post
-	obj.save({ 
-		success: function(obj) { 
+	obj.save({
+		success: function(obj) {
 			console.log('posted to sample app');
 		},
-		error: function(err) { 
+		error: function(err) {
 			console.log("oops!! a problem occured while posting.. please try again.");
 		}
 	});
 	return false;
 });
 
-//CloudSearch Search 
+//CloudSearch Search
 $("#searchForm").submit(function(){
 	var searchObj = new CB.CloudSearch('feed');
-	searchObj.searchOn('post', $('#search').val());
+	searchObj.searchQuery = new CB.SearchQuery();
+	searchObj.searchQuery.searchOn('post', $('#search').val());
 	searchObj.search({
 		success: function(list){
 			$("#feed").empty();
 			if(list.length > 0){
-				for (i = 0; i < list.length; i++) { 
+				for (i = 0; i < list.length; i++) {
 					$("#feed").append("<div class='panel panel-default'><div class='panel-body'><div class='col-md-1 col-sm-2 col-xs-3'><img src='image/profile.png' class='img-responsive img-circle'/></div><div><strong>"+list[i].get('name')+"</strong><br/>"+list[i].get('post')+"</div></div></div>");
 				}
 			} else{
@@ -85,12 +86,8 @@ $("#searchForm").submit(function(){
 			}
 		},
 		error: function(err){
-			
+
 		}
 	});
 	return false;
 });
-
-
-
-
